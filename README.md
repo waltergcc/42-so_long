c7946462c2fb77e603193a89746c3d5672ecf9b4
wcorrea- <waltergccruz@gmail.com>
2023-05-17 02:34:14 +0100

    Add new line between errors

---
+++ README.md
@@ -0,0 +1,5 @@
+# so_long
+
+## Used tests
+so_long_invalidator: https://github.com/Nuno-Jesus/so_long_invalidator
+
--- so_long/src/utils.c
+++ so_long/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: wcorrea- <wcorrea-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 09:48:12 by wcorrea-          #+#    #+#             */
-/*   Updated: 2023/05/14 18:43:46 by wcorrea-         ###   ########.fr       */
+/*   Updated: 2023/05/17 02:25:38 by wcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ int	flood_fill(t_map *map, t_point curr, char **path)
 int	exit_error(t_game *so_long, char *msg)
 {
 	clean_game(so_long);
-	ft_putstr_fd("Error: ", 2);
+	ft_putstr_fd("Error\n", 2);
 	ft_putstr_fd(msg, 2);
 	ft_putstr_fd("\n", 2);
 	exit (EXIT_FAILURE);
