4a5cf2efcaf15a216d9994741f953de237a5e8b6
waltergcc <waltergccruz@gmail.com>
2023-05-17 12:25:15 +0100

    Sove leak problems with mlx destroy functions

--- so_long/src/clean_exit.c
+++ so_long/src/clean_exit.c
@@ -6,7 +6,7 @@
 /*   By: wcorrea- <wcorrea-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 11:28:49 by wcorrea-          #+#    #+#             */
-/*   Updated: 2023/05/14 12:11:46 by wcorrea-         ###   ########.fr       */
+/*   Updated: 2023/05/17 11:43:42 by wcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,30 @@ void	clean_map(t_map *map)
 	free (map);
 }
 
+
+void	clean_sprites(t_game *so_long)
+{
+	int	i;
+
+	i = -1;
+	while (++i < SPRITES)
+		mlx_destroy_image(so_long->display.mlx, so_long->sp[i].img);
+	free (so_long->sp);
+}
+
 void	clean_game(t_game *so_long)
 {
 	if (!so_long)
 		return ;
 	if (so_long->map)
 		clean_map(so_long->map);
+	if (so_long->sp)
+		clean_sprites(so_long);
+	if (so_long->display.win)
+		mlx_destroy_window(so_long->display.mlx, so_long->display.win);
+	if (so_long->display.mlx)
+		mlx_destroy_display(so_long->display.mlx);
+	free(so_long->display.mlx);
 }
 
 int	quit_game(t_game *so_long)
